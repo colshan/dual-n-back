1fa13ee65ed83e5a1558caba5b7452cb39d315a5
colshan <cjshannon3@gmail.com>
2018-04-28 10:08:21 -0400

    declare function and state variables

---
+++ js/app.js
@@ -0,0 +1,49 @@
+const storage = {
+	audioFiles: []
+};
+
+const state = {
+
+	n: 2,
+	positionHistory: [],
+	audioHistory: [],
+	right: 0,
+	wrong: 0
+
+};
+
+const ctrl = {
+
+	next: function () {
+
+	},
+
+	gameLoop: function () {
+
+	},
+
+	controlHandler: function () {
+
+	},
+
+	setUpUserControls: function () {
+
+	},
+
+	init: function () {
+
+
+	}
+};
+
+const presentation = {
+
+	gameOverModal: function () {
+
+	},
+
+	updateScore: function () {
+
+	},
+
+};
