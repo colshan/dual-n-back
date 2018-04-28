ea1beb9d26894fba78e536f3cfa9318263ba9f2e
colshan <cjshannon3@gmail.com>
2018-04-28 11:57:22 -0400

    add jsdoc comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
+/** Object that contains data that is not unique to user session
+ */
 const storage = {
 	audioFiles: []
 };
 
+
+/** Object that contains state variables necessary for game play.
+ */
 const state = {
 
 	n: 2,
@@ -12,37 +17,61 @@ const state = {
 
 };
 
+
+/**. Object that contains business logic of application
+ */
 const ctrl = {
 
+	/** Randomnly select next position and audio clip to play
+	 * Record history in state object.  Call pres.displayNext
+	 * to update DOM.
+	 */
 	next: function () {
 
 	},
 
+	/** Use setInterval to regularly call next function.
+	 * Terminates and clears interval when game is over.
+	 * Cas gameOverModal.
+	 */
 	gameLoop: function () {
 
 	},
 
+	/** Determine if user input corresponds to right or wrong answer.
+	 * Update state variables accordingly.
+	 */
 	controlHandler: function () {
 
 	},
 
+	/** Add event listeners to controls.
+	 *
+	 */
 	setUpUserControls: function () {
 
 	},
 
+	/** Initialize game.  Add event listeners to controls and start
+	 * the game loop.
+	 */
 	init: function () {
-
 
 	}
 };
 
-const presentation = {
+const pres = {
 
-	gameOverModal: function () {
+	/** Play audio clip and modify DOM to display block in correct position
+	 * for a limited amount of time.
+	 */
+	displayNext: function () {
 
 	},
 
-	updateScore: function () {
+	/** Displays modal when game is over that shows game results.
+	 */
+	gameOverModal: function () {
 
 	},
 
